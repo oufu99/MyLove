011a9d53150aa6c73bd1563933d9bf5c6cea7f88
打野的小妞 <oufu99@gmail.com>
2020-02-16 11:55:09 +0800

    aaron commit

---
+++ AHK/打开Vimd和Ahk.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+:: 获取管理员权限
+cd /d %~dp0
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+
+
+
+:: 两个::是注释
+start "" "D:\Common\VimD\vimd.exe"
+:: vid有点问题先关闭再打开
+taskkill /f /im vimd.exe
+start "" "D:\Common\VimD\vimd.exe"
+
+start "" "D:\MyLove\AHK\MyLove.ahk"
+exit
